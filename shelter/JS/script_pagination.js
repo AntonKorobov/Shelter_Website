110372819154a4a3b31d8dc503e01c2cb44d1d0d
NewAnton <anton1korobov1@gmail.com>
2023-03-08 16:28:38 +0300

    refactor: correct css in pets page

--- shelter/JS/script_pagination.js
+++ shelter/JS/script_pagination.js
@@ -1,15 +1,15 @@
 // let CARDS = document.querySelectorAll('.card');
-const PAGINATOR = document.querySelector(".cards_box");
-const BTN_PAGE = document.querySelector(".paginator_page");
-const BTN_LEFT = document.querySelector(".paginator_left");
-const BTN_RIGHT = document.querySelector(".paginator_right");
-const BTN_START = document.querySelector(".paginator_start");
-const BTN_FINISH = document.querySelector(".paginator_finish");
+const PAGINATOR = document.querySelector('.cards-box');
+const BTN_PAGE = document.querySelector('.paginator-page');
+const BTN_LEFT = document.querySelector('.paginator-left');
+const BTN_RIGHT = document.querySelector('.paginator-right');
+const BTN_START = document.querySelector('.paginator-start');
+const BTN_FINISH = document.querySelector('.paginator-finish');
 
-BTN_LEFT.addEventListener("click", pageLeft);
-BTN_RIGHT.addEventListener("click", pageRight);
-BTN_START.addEventListener("click", pageStart);
-BTN_FINISH.addEventListener("click", pageFinish);
+BTN_LEFT.addEventListener('click', pageLeft);
+BTN_RIGHT.addEventListener('click', pageRight);
+BTN_START.addEventListener('click', pageStart);
+BTN_FINISH.addEventListener('click', pageFinish);
 
 import { showModalWindowEvent } from '../JS/script_popup.js';
 
@@ -20,116 +20,117 @@ const response = await fetch('../../base/pets_base.json');
 const cardBase = await response.json();
 
 function pageLeft() {
-    if (currentPage == 1) return
-    currentPage--;
+  if (currentPage == 1) return;
+  currentPage--;
 
-    closeCard();
+  closeCard();
 
-    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener("animationend", () => { //maybe change when the last card animated?
-        PAGINATOR.innerHTML = "";
-        PAGINATOR.innerHTML = listOfPages[currentPage - 1].join("");
-        BTN_PAGE.innerHTML = currentPage;
-        BTN_RIGHT.classList.add("navigation_button_active");
-        BTN_FINISH.classList.add("navigation_button_active");
-        if (currentPage == 1) {
-            BTN_LEFT.classList.remove("navigation_button_active");
-            BTN_START.classList.remove("navigation_button_active");
-        }
-        showModalWindowEvent();
+  document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener('animationend', () => {
+    //maybe change when the last card animated?
+    PAGINATOR.innerHTML = '';
+    PAGINATOR.innerHTML = listOfPages[currentPage - 1].join('');
+    BTN_PAGE.innerHTML = currentPage;
+    BTN_RIGHT.classList.add('navigation-button_active');
+    BTN_FINISH.classList.add('navigation-button_active');
+    if (currentPage == 1) {
+      BTN_LEFT.classList.remove('navigation-button_active');
+      BTN_START.classList.remove('navigation-button_active');
+    }
+    showModalWindowEvent();
 
-        openCard();
-    });
+    openCard();
+  });
 }
 
 function pageStart() {
-    if (currentPage == 1) return
-    currentPage = 1;
+  if (currentPage == 1) return;
+  currentPage = 1;
 
-    closeCard();
+  closeCard();
 
-    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener("animationend", () => {
-        PAGINATOR.innerHTML = "";
-        PAGINATOR.innerHTML = listOfPages[currentPage - 1].join("");
-        BTN_PAGE.innerHTML = currentPage;
-        BTN_LEFT.classList.remove("navigation_button_active");
-        BTN_START.classList.remove("navigation_button_active");
-        BTN_RIGHT.classList.add("navigation_button_active");
-        BTN_FINISH.classList.add("navigation_button_active");
-        showModalWindowEvent();
+  document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener('animationend', () => {
+    PAGINATOR.innerHTML = '';
+    PAGINATOR.innerHTML = listOfPages[currentPage - 1].join('');
+    BTN_PAGE.innerHTML = currentPage;
+    BTN_LEFT.classList.remove('navigation-button_active');
+    BTN_START.classList.remove('navigation-button_active');
+    BTN_RIGHT.classList.add('navigation-button_active');
+    BTN_FINISH.classList.add('navigation-button_active');
+    showModalWindowEvent();
 
-        openCard();
-    });
+    openCard();
+  });
 }
 
 function pageRight() {
-    if (currentPage == listOfPages.length) return
-    BTN_LEFT.classList.add("navigation_button_active");
-    BTN_START.classList.add("navigation_button_active");
-    currentPage++;
+  if (currentPage == listOfPages.length) return;
+  BTN_LEFT.classList.add('navigation-button_active');
+  BTN_START.classList.add('navigation-button_active');
+  currentPage++;
 
-    closeCard();
+  closeCard();
 
-    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener("animationend", () => {
-        PAGINATOR.innerHTML = "";
-        PAGINATOR.innerHTML = listOfPages[currentPage - 1].join("");
-        BTN_PAGE.innerHTML = currentPage;
-        if (currentPage == listOfPages.length) {
-            BTN_RIGHT.classList.remove("navigation_button_active");
-            BTN_FINISH.classList.remove("navigation_button_active");
-        }
-        showModalWindowEvent();
+  document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener('animationend', () => {
+    PAGINATOR.innerHTML = '';
+    PAGINATOR.innerHTML = listOfPages[currentPage - 1].join('');
+    BTN_PAGE.innerHTML = currentPage;
+    if (currentPage == listOfPages.length) {
+      BTN_RIGHT.classList.remove('navigation-button_active');
+      BTN_FINISH.classList.remove('navigation-button_active');
+    }
+    showModalWindowEvent();
 
-        openCard();
-    })
+    openCard();
+  });
 }
 
 function pageFinish() {
-    if (currentPage == listOfPages.length) return
-    currentPage = listOfPages.length;
+  if (currentPage == listOfPages.length) return;
+  currentPage = listOfPages.length;
 
-    closeCard();
+  closeCard();
 
-    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener("animationend", () => {
-        PAGINATOR.innerHTML = "";
-        PAGINATOR.innerHTML = listOfPages[currentPage - 1].join("");
-        BTN_PAGE.innerHTML = currentPage;
-        BTN_LEFT.classList.add("navigation_button_active");
-        BTN_START.classList.add("navigation_button_active");
-        BTN_RIGHT.classList.remove("navigation_button_active");
-        BTN_FINISH.classList.remove("navigation_button_active");
-        showModalWindowEvent();
+  document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].addEventListener('animationend', () => {
+    PAGINATOR.innerHTML = '';
+    PAGINATOR.innerHTML = listOfPages[currentPage - 1].join('');
+    BTN_PAGE.innerHTML = currentPage;
+    BTN_LEFT.classList.add('navigation-button_active');
+    BTN_START.classList.add('navigation-button_active');
+    BTN_RIGHT.classList.remove('navigation-button_active');
+    BTN_FINISH.classList.remove('navigation-button_active');
+    showModalWindowEvent();
 
-        openCard();
-    });
+    openCard();
+  });
 }
 
 function closeCard() {
-    let stepTimer = 0;
-    document.querySelectorAll('.card').forEach(item => {
-        setTimeout(function() {
-            item.classList.add("card_close-flip");
-        }, stepTimer);
-        stepTimer += 50;
-    })
+  let stepTimer = 0;
+  document.querySelectorAll('.card').forEach((item) => {
+    setTimeout(function () {
+      item.classList.add('card_close-flip');
+    }, stepTimer);
+    stepTimer += 50;
+  });
 }
 
 function openCard() {
-    let stepTimer = 0;
-    document.querySelectorAll('.card').forEach(item => {
-        item.classList.add("card_close");
-        setTimeout(function() {
-            item.classList.add("card_open-flip");
-            item.addEventListener("animationend", removeAnimation);
+  let stepTimer = 0;
+  document.querySelectorAll('.card').forEach((item) => {
+    item.classList.add('card_close');
+    setTimeout(function () {
+      item.classList.add('card_open-flip');
+      item.addEventListener('animationend', removeAnimation);
 
-            function removeAnimation() {
-                item.classList.remove("card_open-flip");
-                item.classList.remove("card_close-flip");
-                item.removeEventListener("animationend", removeAnimation);
-                item.classList.remove("card_close");
-            }
-        }, stepTimer);
-        stepTimer += 50;
-    })
+      function removeAnimation() {
+        item.classList.remove('card_open-flip');
+        item.classList.remove('card_close-flip');
+        item.removeEventListener('animationend', removeAnimation);
+        item.classList.remove('card_close');
+      }
+    }, stepTimer);
+    stepTimer += 50;
+  });
 }
 
 const listOfPages = [];
@@ -139,56 +140,56 @@ BTN_PAGE.innerHTML = currentPage;
 
 //GENERATE PAGES. SHOW FIRST PAGE-----------------------------------------------
 if (window.matchMedia('(min-width: 1024px)').matches) {
-    PAGINATOR.innerHTML = (generateMatrixOfCards(8)[0].join(""));
+  PAGINATOR.innerHTML = generateMatrixOfCards(8)[0].join('');
 }
 if (window.matchMedia('(max-width: 1023px) and (min-width: 750px)').matches) {
-    PAGINATOR.innerHTML = (generateMatrixOfCards(6)[0].join(""));
+  PAGINATOR.innerHTML = generateMatrixOfCards(6)[0].join('');
 }
 if (window.matchMedia('(min-width: 320px) and (max-width: 749px)').matches) {
-    PAGINATOR.innerHTML = (generateMatrixOfCards(3)[0].join(""));
+  PAGINATOR.innerHTML = generateMatrixOfCards(3)[0].join('');
 }
 showModalWindowEvent();
 //------------------------------------------------------------------------------
 
 function generateMatrixOfCards(cardsOnPage) {
-    const listOfCards = [];
-    const numberOfCards = 48;
-    const numberOfPages = Math.ceil(numberOfCards / cardsOnPage);
+  const listOfCards = [];
+  const numberOfCards = 48;
+  const numberOfPages = Math.ceil(numberOfCards / cardsOnPage);
 
-    // Generate cards
-    let cardCounter = 0;
-    for (let i = 0; i < numberOfCards; i++) {
-        if (cardCounter == 8) cardCounter = 0;
-        let nameOfPet = cardBase[cardCounter].name;
-        let imageOfPet = cardBase[cardCounter].img;
-        listOfCards.push(`<div class="card"><img src="${imageOfPet}" alt=""><p>${nameOfPet}</p><a class="card__button">Learn more</a></div>`);
-        cardCounter++;
-    } //Can be optimized. Generate half of array and then add copy//!!!!!!!!
+  // Generate cards
+  let cardCounter = 0;
+  for (let i = 0; i < numberOfCards; i++) {
+    if (cardCounter == 8) cardCounter = 0;
+    let nameOfPet = cardBase[cardCounter].name;
+    let imageOfPet = cardBase[cardCounter].img;
+    listOfCards.push(`<div class="card"><img src="${imageOfPet}" alt=""><p>${nameOfPet}</p><a class="card__button">Learn more</a></div>`);
+    cardCounter++;
+  } //Can be optimized. Generate half of array and then add copy//!!!!!!!!
 
-    // Generate pages
-    for (let currPage = 1; currPage <= numberOfPages; currPage++) {
-        let trimStart = (currPage - 1) * cardsOnPage;
-        let trimEnd = trimStart + cardsOnPage;
-        listOfPages[currPage - 1] = listOfCards.slice(trimStart, trimEnd);
+  // Generate pages
+  for (let currPage = 1; currPage <= numberOfPages; currPage++) {
+    let trimStart = (currPage - 1) * cardsOnPage;
+    let trimEnd = trimStart + cardsOnPage;
+    listOfPages[currPage - 1] = listOfCards.slice(trimStart, trimEnd);
+  }
+
+  //Shuffle pages
+  for (let i = 0; i < numberOfPages; i++) {
+    shuffle(listOfPages[i]);
+  }
+
+  //Shuffle cards
+  shuffle(listOfPages);
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+  }
 
-    //Shuffle pages
-    for (let i = 0; i < numberOfPages; i++) {
-        shuffle(listOfPages[i]);
-    }
+  // showModalWindowEvent(); //doesn't work
+  // CARDS = document.querySelectorAll('.card'); //addEvent click every time after generation
 
-    //Shuffle cards
-    shuffle(listOfPages);
-
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-    // showModalWindowEvent(); //doesn't work
-    // CARDS = document.querySelectorAll('.card'); //addEvent click every time after generation
-
-    return listOfPages;
+  return listOfPages;
 }
--- shelter/JS/script_popup.js
+++ shelter/JS/script_popup.js
@@ -1,14 +1,14 @@
-const WINDOW = document.querySelector("body");
-const MODAL_WINDOW_BACKGROUND = document.querySelector('.modal-window-background');
-const MODAL_WINDOW = document.querySelector('.modal-window-wrapper');
-const PET_NAME = document.querySelector('.modal-window__content_pet-name');
+const WINDOW = document.querySelector('body');
+const MODAL_WINDOW_BACKGROUND = document.querySelector('.modal-window__background');
+const MODAL_WINDOW = document.querySelector('.modal-window__wrapper');
+const PET_NAME = document.querySelector('.content__pet-name');
 const PET_IMAGE = document.querySelector('.modal-window__pet-image');
-const PET_BREAD = document.querySelector('.modal-window__content_pet-bread');
-const PET_DESCRIPTION = document.querySelector('.modal-window__content_pet-description');
-const PET_AGE = document.querySelector('.modal-window__content_pet-age');
-const PET_INOCULATIONS = document.querySelector('.modal-window__content_pet-inoculations');
-const PET_DISEASES = document.querySelector('.modal-window__content_pet-diseases');
-const PET_PARASITES = document.querySelector('.modal-window__content_pet-parasites');
+const PET_BREAD = document.querySelector('.content__pet-bread');
+const PET_DESCRIPTION = document.querySelector('.content__pet-description');
+const PET_AGE = document.querySelector('.content__pet-age');
+const PET_INOCULATIONS = document.querySelector('.content__pet-inoculations');
+const PET_DISEASES = document.querySelector('.content__pet-diseases');
+const PET_PARASITES = document.querySelector('.content__pet-parasites');
 const EXIT_BUTTON = document.querySelector('.modal-window__exit-button');
 
 const response = await fetch('../../base/pets_base.json');
@@ -18,51 +18,51 @@ MODAL_WINDOW_BACKGROUND.addEventListener('click', closeModalWindow);
 EXIT_BUTTON.addEventListener('click', closeModalWindow);
 
 export function showModalWindowEvent() {
-    document.querySelectorAll('.card').forEach(item => {
-        item.addEventListener('click', event => {
-            showInformationByName(item);
-            WINDOW.classList.add("scroll_lock");
-            MODAL_WINDOW.classList.add("modal-window_active");
-            MODAL_WINDOW_BACKGROUND.classList.add("modal-window-background_active");
-        })
-    })
+  document.querySelectorAll('.card').forEach((item) => {
+    item.addEventListener('click', (event) => {
+      showInformationByName(item);
+      WINDOW.classList.add('scroll_lock');
+      MODAL_WINDOW.classList.add('modal-window_active');
+      MODAL_WINDOW_BACKGROUND.classList.add('modal-window-background_active');
+    });
+  });
 }
 
 function closeModalWindow() {
-    WINDOW.classList.remove("scroll_lock");
-    MODAL_WINDOW_BACKGROUND.classList.remove("modal-window-background_active");
-    MODAL_WINDOW.classList.remove("modal-window_active");
+  WINDOW.classList.remove('scroll_lock');
+  MODAL_WINDOW_BACKGROUND.classList.remove('modal-window-background_active');
+  MODAL_WINDOW.classList.remove('modal-window_active');
 }
 
 function showInformationByName(item) {
-    let petName = item.children[1].innerText;
+  let petName = item.children[1].innerText;
 
-    let petBaseCardNumber;
-    for (let i = 0; i < cardBase.length; i++) {
-        if (cardBase[i]["name"] == petName) {
-            petBaseCardNumber = i;
-            break
-        }
+  let petBaseCardNumber;
+  for (let i = 0; i < cardBase.length; i++) {
+    if (cardBase[i]['name'] == petName) {
+      petBaseCardNumber = i;
+      break;
     }
+  }
 
-    let petImgLink = cardBase[petBaseCardNumber]["img"];
-    let petType = cardBase[petBaseCardNumber]["type"];
-    let petBread = cardBase[petBaseCardNumber]["breed"];
-    let petDescription = cardBase[petBaseCardNumber]["description"];
-    let petAge = cardBase[petBaseCardNumber]["age"];
-    let petInoculations = cardBase[petBaseCardNumber]["inoculations"];
-    let petDiseases = cardBase[petBaseCardNumber]["diseases"];
-    let petParasites = cardBase[petBaseCardNumber]["parasites"];
+  let petImgLink = cardBase[petBaseCardNumber]['img'];
+  let petType = cardBase[petBaseCardNumber]['type'];
+  let petBread = cardBase[petBaseCardNumber]['breed'];
+  let petDescription = cardBase[petBaseCardNumber]['description'];
+  let petAge = cardBase[petBaseCardNumber]['age'];
+  let petInoculations = cardBase[petBaseCardNumber]['inoculations'];
+  let petDiseases = cardBase[petBaseCardNumber]['diseases'];
+  let petParasites = cardBase[petBaseCardNumber]['parasites'];
 
-    PET_IMAGE.src = petImgLink;
-    PET_NAME.innerHTML = petName;
-    PET_BREAD.innerHTML = petType + " - " + petBread;
-    PET_DESCRIPTION.innerHTML = petDescription;
-    PET_AGE.innerHTML = "<b>Age: </b>" + petAge;
-    PET_INOCULATIONS.innerHTML = "<b>Inoculation: </b>" + petInoculations;
-    PET_DISEASES.innerHTML = "<b>Diseases: </b>" + petDiseases;
-    PET_PARASITES.innerHTML = "<b>Parasites: </b>" + petParasites;
-    // if (window.matchMedia('(min-width: 320px) and (max-width: 749px)').matches) {
-    //     PET_IMAGE.src = "";
-    // }
-};
+  PET_IMAGE.src = petImgLink;
+  PET_NAME.innerHTML = petName;
+  PET_BREAD.innerHTML = petType + ' - ' + petBread;
+  PET_DESCRIPTION.innerHTML = petDescription;
+  PET_AGE.innerHTML = '<b>Age: </b>' + petAge;
+  PET_INOCULATIONS.innerHTML = '<b>Inoculation: </b>' + petInoculations;
+  PET_DISEASES.innerHTML = '<b>Diseases: </b>' + petDiseases;
+  PET_PARASITES.innerHTML = '<b>Parasites: </b>' + petParasites;
+  // if (window.matchMedia('(min-width: 320px) and (max-width: 749px)').matches) {
+  //     PET_IMAGE.src = "";
+  // }
+}
